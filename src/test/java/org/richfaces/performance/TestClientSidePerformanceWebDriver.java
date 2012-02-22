b3d0c273eed703b95aa9220b19283af3fbdffe11
Juraj Huska <jhuska@redhat.com>
2012-02-22 16:28:03 +0100

    correct method for posting har files added

--- src/test/java/org/richfaces/performance/TestClientSidePerformanceWebDriver.java
+++ src/test/java/org/richfaces/performance/TestClientSidePerformanceWebDriver.java
@@ -1,16 +1,12 @@
 package org.richfaces.performance;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 
 import org.browsermob.core.har.Har;
@@ -19,24 +15,21 @@ import org.openqa.selenium.Proxy;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestClientSidePerformanceWebDriver {
 
-    // @Drone
     protected FirefoxDriver webDriver;
-    //
     protected ProxyServer server;
 
-    // @Deployment(testable = false)
-    // public static WebArchive createTestArchive() {
-    //
-    // WebArchive war = ShrinkWrap.createFromZipFile(WebArchive.class, new File("target/showcase.war"));
-    // return war;
-    // }
+    protected final String HOST = "localhost";
+    protected final String PORT = "5000";
+    protected final String PATH = "/results/upload";
+    protected final String METHOD = "POST";
 
+    protected final String CORRECT_HAR = "exampleHar.har";
+        
     @BeforeClass
     public void startProxyServer() throws Exception {
 
@@ -50,118 +43,77 @@ public class TestClientSidePerformanceWebDriver {
         webDriver = new FirefoxDriver(capabilities);
     }
 
-//    @AfterClass
-//    public void postHarToStorage() {
-//        secondMethodPost();
-//    }
+    public String send(String data) throws Exception {
 
-    private void secondMethodPost(Har har) {
-        try {
-            URL url = new URL("http://localhost:5000/results/upload");
-            HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+        // Connection
+        URL url = new URL("http://" + HOST + ":" + PORT + PATH);
 
-            StringBuffer content = new StringBuffer();
-            content.append(URLEncoder.encode("file", "UTF-8"));
-//            content.append("file:");
-            
-//            File harFile = new File("exampleHar.har");
-//            BufferedReader br = new BufferedReader(new FileReader(harFile));
-//            String line = br.readLine();
-//            while (line != null) {
-//                content.append(URLEncoder.encode(line, "UTF-8"));
-////                content.append(line);
-//                line = br.readLine();
-//            }
-            
-            httpCon.setRequestMethod("POST");
-//            httpCon.setRequestProperty("Content-length", "33");
-            httpCon.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
-            httpCon.setRequestProperty("Automated", "true");
-            
-            httpCon.setDoInput(true);
-            httpCon.setDoOutput(true);
-          
-            //Send request
-            DataOutputStream wr = new DataOutputStream (
-                        httpCon.getOutputStream ());
-            
-            har.writeTo(wr);
-            wr.flush();
-            wr.close();
-            
-            OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
-            System.out.println(httpCon.getResponseCode());
-            System.out.println(httpCon.getResponseMessage());
-            out.close();
-            
-        } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod(METHOD);
+
+        connection.setDoOutput(true);
+
+        // HTTP headers
+        connection.setRequestProperty("Automated", "true");
+
+        // Send data to server
+        OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream());
+
+        data = URLEncoder.encode(data, "utf-8");
+
+        wr.write("file=" + data);
+        wr.flush();
+        wr.close();
+
+        // Get the response
+        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        String response = "";
+        while ((line = rd.readLine()) != null) {
+            response += line;
         }
+
+        rd.close();
+
+        connection.disconnect();
+
+        return response;
     }
 
-//    private void firstMethodPost() {
-//        try {
-//            URL url = new URL("http://localhost:5000/results/upload");
-//            URLConnection conn = url.openConnection();
-//            conn.setDoInput(true);
-//            conn.setDoOutput(true);
-//            conn.setUseCaches(false);
-//            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-//            conn.setRequestProperty("Automated", "true");
-//
-//            DataOutputStream writer = new DataOutputStream(conn.getOutputStream());
-//
-//            StringBuffer content = new StringBuffer();
-//            content.append(URLEncoder.encode("file=", "UTF-8"));
-//            File harFile = new File(HAR);
-//            BufferedReader br = new BufferedReader(new FileReader(harFile));
-//            String line = br.readLine();
-//            while (line != null) {
-//                content.append(URLEncoder.encode(line, "UTF-8"));
-//                line = br.readLine();
-//            }
-//
-//            writer.writeBytes(content.toString());
-//            writer.flush();
-//            writer.close();
-//
-//            StringBuffer answer = new StringBuffer();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//            while ((line = reader.readLine()) != null) {
-//                answer.append(line);
-//            }
-//
-//            reader.close();
-//
-//            System.out.println("################" + answer);
-//
-//        } catch (MalformedURLException ex) {
-//            ex.printStackTrace();
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
-//    }
+    private void sendHar(File file) throws Exception {
+        FileInputStream fis = new FileInputStream(file);
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(fis));
 
+        StringBuffer sbf = new StringBuffer();
+        String line = bfr.readLine();
+        if (line != null) {
+            sbf.append(line);
+            line = bfr.readLine();
+        }
+
+        System.out.println("Response: " + send(sbf.toString()));
+    }
+    
     @Test
-    public void testClientSidePerformance() throws IOException {
+    public void testClientSidePerformance() throws Exception {
 
-        server.newHar("" + System.currentTimeMillis());
+        server.newHar("showcase");
 
         webDriver
             .get("http://showcase.richfaces.org/richfaces/component-sample.jsf?demo=dataTable&sample=tableSorting&skin=blueSky");
 
-//        webDriver.findElementByLinkText("Sort by Capital Name").click();
-//        webDriver.findElementByLinkText("Sort by State Name").click();
+        // webDriver.findElementByLinkText("Sort by Capital Name").click();
+        // webDriver.findElementByLinkText("Sort by State Name").click();
 
         Har har = server.getHar();
-        secondMethodPost(har);
         File saveHar = new File(System.currentTimeMillis() + ".har");
         har.writeTo(saveHar);
         
+        //send correct har
+//        sendHar(new File(CORRECT_HAR));
+        
+        sendHar(saveHar);
     }
 
 }
